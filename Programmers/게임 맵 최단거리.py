5a2ee8d58322884d4d5d91c02e244736b32e6eb9
WooyoungKwon <rnjsdndud00@inha.edu>
2024-08-07 21:21:31 +0900

    전력망 둘로 나누기

--- Programmers/게임 맵 최단거리.py
+++ Programmers/게임 맵 최단거리.py
@@ -1,29 +1,33 @@
 from collections import deque
-def solution(maps):
-        answer = 0
-        # 상하좌우
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
+def solution(n, wires):
+    answer = n
+    # wires를 2차원 배열로 표현, 1번 송전탑부터 시작이기 때문에 n+1
+    tree = [[] for _ in range(n+1)]
+    # 송전탑 간의 간선을 트리로 구현
+    for v1, v2 in wires:
+        tree[v1].append(v2)
+        tree[v2].append(v1)
 
-        queue = deque()
-        # 0, 0에서 시작한다.
-        queue.append((0,0))
-        
-        while queue:
-                x, y = queue.popleft()
-                for i in range(4):
-                        nx = x + dx[i]
-                        ny = y + dy[i]
-                        # 가로, 세로 길이
-                        N = len(maps)
-                        M = len(maps[0])
-                        if 0<=nx<N and 0<=ny<M and maps[nx][ny] == 1:
-                                # 방문한 곳은 1로 바꿔줌
-                                maps[nx][ny] = maps[x][y] + 1
-                                # 방문한 곳을 큐에 추가하여 다음 반복 때 기준으로 삼고 주변 노드 탐색
-                                queue.append((nx, ny))
-        answer = maps[N-1][M-1]
+    for start, split in wires:
+        visited = [False] * (n+1)
+        # bfs는 모든 노드를 순회하여 count를 세는 방식인데, 간선을 끊기 위해 split 노드를 이미 방문함으로 처리하여 bfs에 내에서 순회하지 않는 방식으로 간선을 끊었음.
+        visited[split] = True
+        cnt = bfs(start, visited, tree)
+        if answer > abs(cnt - (n - cnt)):
+            answer = abs(cnt - (n - cnt))
+    return answer
 
-        if answer == 1:
-                answer = -1
-        return answer
+def bfs(start, visited, tree):
+    cnt = 1
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        for i in tree[v]:
+            if visited[i]:
+                continue
+            queue.append(i)
+            visited[i] = True
+            cnt += 1
+    return cnt
+print(solution(9, [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
